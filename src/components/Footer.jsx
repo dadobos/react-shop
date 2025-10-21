885b1d9744fb1e404c64a43e02b1cc62955cf7b0
Dan Dobos <dan@prowarehouse.nl>
2025-10-21 09:11:20 +0200

    Updates

--- src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-const Footer = () => {
-	const getCurrentYear = () => {
-		return new Date().getFullYear();
-	};
-
-	return <div className='footer'>Dan Dobos &copy; {getCurrentYear()}</div>;
-};
-
-export default Footer;
